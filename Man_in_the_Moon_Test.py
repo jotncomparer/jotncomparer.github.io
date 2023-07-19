3213b98e9ab98ceb854934698b84a37ee20b8d78
Man in the Moon <139192900+Man-in-the-Moon@users.noreply.github.com>
2023-07-19 12:32:50 -0400

    Update Man_in_the_Moon_Test.py
    Added total value calculations for both dungeons and raids. Added function Time_Converter() that converts total seconds of any value into weeks, days, hours, minutes, and seconds.

--- Man_in_the_Moon_Test.py
+++ Man_in_the_Moon_Test.py
@@ -11,14 +11,35 @@ base_auth_url = "https://www.bungie.net/en/OAuth/Authorize"
 redirect_url = "https://jotncomparer.github.io/"
 token_url = "https://www.bungie.net/platform/app/oauth/token"
 
+def Time_Converter(time):
+    Seconds = time % 60
+    Minutes = time / 60
+    Minutes = math.trunc(Minutes)
+    Hours = Minutes / 60
+    Hours = math.trunc(Hours)
+    Minutes = Minutes % 60
+    Days = Hours / 24
+    Days = math.trunc(Days)
+    Hours = Hours % 24
+    Weeks = Days / 7
+    Weeks = math.trunc(Weeks)
+    Days = Days % 7
+    print(f'Total Time: {Weeks} week(s), {Days} day(s), {Hours}:{Minutes}:{Seconds}')
+
 # membershipType: Designates what machine the player uses. This number comes from the first device the account is set
 # on. 1 - Xbox, 2- Playstation, 3 - Steam
 # destinyMembershipId: The actual user account
 # characterId: The specific character (Titan/Warlock/Hunter) that is used by the membership account
+
 # Man in the Moon - Connor
 membershipType = 1
 destinyMembershipId = 4611686018450697084
+# Titan:
 characterId = 2305843009644414176
+# Warlock:
+# characterId = 2305843009663894341
+# Hunter:
+# characterId = 2305843009703275457
 
 # The Chrome Leaf - Thomas
 # membershipType = 1
@@ -30,6 +51,66 @@ characterId = 2305843009644414176
 # Hunter:
 # characterId = 2305843009569534739
 
+# Piepuns - Douglas
+# membershipType = 1
+# destinyMembershipId = 4611686018434621591
+# Warlock:
+# characterId = 2305843009802904121
+# Titan:
+# characterId = 2305843009668854600
+# Hunter:
+# characterId = 2305843009348154555
+
+# TheZefraOracle - Mark
+# membershipType = 1
+# destinyMembershipId = 4611686018450697084
+# Warlock:
+# characterId = 2305843009802904121
+# Titan:
+# characterId = 2305843009668854600
+# Hunter:
+# characterId = 2305843009348154555
+
+# HeavyChevy - Jack
+# membershipType = 2
+# destinyMembershipId = 4611686018469231992
+# Warlock:
+# characterId = 2305843009891864023
+# Titan:
+# characterId = 2305843009890274343
+# Hunter:
+# characterId = 2305843009268771475
+
+# Lachlan - Hunter
+# membershipType = 3
+# destinyMembershipId = 4611686018476416864
+# Warlock:
+# characterId = 2305843009359365362
+# Titan:
+# characterId = 2305843009756404411
+# Hunter:
+# characterId = 2305843009359734078
+
+# SlayWarsV - Cameron
+# membershipType = 3
+# destinyMembershipId = 4611686018501646188
+# Warlock:
+# characterId = 2305843009683284493
+# Titan:
+# characterId = 2305843009624174508
+# Hunter:
+# characterId = 2305843009683284492
+
+# Gargoyle Goose - Kade
+# membershipType = 1
+# destinyMembershipId = 4611686018451886498
+# Warlock:
+# characterId = 2305843009264637524
+# Titan:
+# characterId = 2305843009264637527
+# Hunter:
+# characterId = 2305843010322954573
+
 # MODE Numbers: 4 = Raids, 82 = Dungeons
 # COUNT Limit: 250 per PAGE
 # PAGE LIMIT: unknown
@@ -38,6 +119,7 @@ MODE = 82
 COUNT = 250
 PAGE = 0
 
+# Dungeon Values for Connor
 if MODE == 82 and destinyMembershipId == 4611686018450697084:
 
     # All Dungeon Data
@@ -54,6 +136,14 @@ if MODE == 82 and destinyMembershipId == 4611686018450697084:
     Proph_Start = 0
     Grasp_Start = 0
     Duality_Start = 0
+
+    Spire_Comp = 0
+    GotD_Comp = 0
+    ST_Comp = 0
+    Pit_Comp = 0
+    Proph_Comp = 0
+    Grasp_Comp = 0
+    Duality_Comp = 0
 
     Spire_Tot_Time = 0
     GotD_Tot_Time = 0
@@ -213,36 +303,57 @@ if MODE == 82 and destinyMembershipId == 4611686018450697084:
 
         if Name == 1262462921 or Name == 1801496203:
             Name = "Spire of the Watcher"
+            Spire_Start += 1
+            if Completion == 1:
+                Spire_Comp += 1
             Spire_Tot_Time += Time
             Spire_Tot_K += Kills
             Spire_Tot_D += Deaths
         elif Name == 2823159265 or Name == 1668217731:
             Name = "Duality"
+            Duality_Start += 1
+            if Completion == 1:
+                Duality_Comp += 1
             Duality_Tot_Time += Time
             Duality_Tot_K += Kills
             Duality_Tot_D += Deaths
         elif Name == 4078656646 or Name == 3774021532:
             Name = "Grasp of Avarice"
+            Grasp_Start += 1
+            if Completion == 1:
+                Grasp_Comp += 1
             Grasp_Tot_Time += Time
             Grasp_Tot_K += Kills
             Grasp_Tot_D += Deaths
         elif Name == 313828469 or Name == 2716998124:
             Name = "Ghosts of the Deep"
+            GotD_Start += 1
+            if Completion == 1:
+                GotD_Comp += 1
             GotD_Tot_Time += Time
             GotD_Tot_K += Kills
             GotD_Tot_D += Deaths
         elif Name == 2032534090:
             Name = "The Shattered Throne"
+            ST_Start += 1
+            if Completion == 1:
+                ST_Comp += 1
             ST_Tot_Time += Time
             ST_Tot_K += Kills
             ST_Tot_D += Deaths
         elif Name == 2582501063 or Name == 1375089621:
             Name = "Pit of Heresy"
+            Pit_Start += 1
+            if Completion == 1:
+                Pit_Comp += 1
             Pit_Tot_Time += Time
             Pit_Tot_K += Kills
             Pit_Tot_D += Deaths
         elif Name == 4148187374 or Name == 1077850348:
             Name = "Prophecy"
+            Proph_Start += 1
+            if Completion == 1:
+                Proph_Comp += 1
             Proph_Tot_Time += Time
             Proph_Tot_K += Kills
             Proph_Tot_D += Deaths
@@ -254,7 +365,7 @@ if MODE == 82 and destinyMembershipId == 4611686018450697084:
             if Players == 1:
                 Completion = "Solo"
                 if Deaths == 0:
-                    Completion = "Solo FLawless"
+                    Completion = "Solo Flawless"
         else:
             Completion = "Activity Incompolete"
 
@@ -265,25 +376,33 @@ if MODE == 82 and destinyMembershipId == 4611686018450697084:
 
         Dungeons = (Name, Kills, Deaths, KD, KAD, Time, Players, Completion)
 
-        print(Dungeons)
+        All_Starts = (ST_Start, Pit_Start, Proph_Start, Grasp_Start, Duality_Start, Spire_Start, GotD_Start)
+        All_Comps = (ST_Comp, Pit_Comp, Proph_Comp, Grasp_Comp, Duality_Comp, Spire_Comp, GotD_Comp)
+        # All_Tot_Time = (ST_Tot_Time, Pit_Tot_Time, Proph_Tot_Time, Grasp_Tot_Time, Duality_Tot_Time,
+        #                Spire_Tot_Time, GotD_Tot_Time)
+        All_Tot_K = (round(ST_Tot_K), round(Pit_Tot_K), round(Proph_Tot_K), round(Grasp_Tot_K),
+                     round(Duality_Tot_K), round(Spire_Tot_K), round(GotD_Tot_K))
+        All_Tot_D = (round(ST_Tot_D), round(Pit_Tot_D), round(Proph_Tot_D), round(Grasp_Tot_D),
+                     round(Duality_Tot_D), round(Spire_Tot_D), round(GotD_Tot_D))
 
-    Seconds = Dungeon_Tot_Time % 60
-    Minutes = Dungeon_Tot_Time / 60
-    Minutes = math.trunc(Minutes)
-    Hours = Minutes / 60
-    Hours = math.trunc(Hours)
-    Minutes = Minutes % 60
-    Days = Hours / 24
-    Days = math.trunc(Days)
-    Hours = Hours % 24
-    Weeks = Days / 7
-    Weeks = math.trunc(Weeks)
-    Days = Days % 7
+        All_Stats = (f'Dungeon Starts: {All_Starts}\nDungeon Finished: {All_Comps}\n'
+                     f'Total Dungeon Kills: {All_Tot_K}\nTotal Dungeon Deaths: {All_Tot_D}')
 
-    Dungeon_Tot_Time = (f'{Weeks} week(s), {Days} day(s), {Hours}:{Minutes}:{Seconds}')
+        # print(Dungeons)
 
-    print(Dungeon_Tot_Time, Dungeon_Tot_Kills, Dungeon_Tot_Deaths)
+    Time_Converter(Dungeon_Tot_Time)
 
+    print(f'Total Kills: {round(Dungeon_Tot_Kills)}\nTotal Deaths: {round(Dungeon_Tot_Deaths)}')
+    print(All_Stats)
+    Time_Converter(ST_Tot_Time)
+    Time_Converter(Pit_Tot_Time)
+    Time_Converter(Proph_Tot_Time)
+    Time_Converter(Grasp_Tot_Time)
+    Time_Converter(Duality_Tot_Time)
+    Time_Converter(Spire_Tot_Time)
+    Time_Converter(GotD_Tot_Time)
+
+# Raid Values for Connor
 elif MODE == 4 and destinyMembershipId == 4611686018450697084:
 
     # All Raid Data
@@ -302,6 +421,16 @@ elif MODE == 4 and destinyMembershipId == 4611686018450697084:
     Vow_Start = 0
     KF_Start = 0
     Root_Start = 0
+
+    Levi_Comp = 0
+    SotP_Comp = 0
+    LW_Comp = 0
+    GoS_Comp = 0
+    DSC_Comp = 0
+    VoG_Comp = 0
+    Vow_Comp = 0
+    KF_Comp = 0
+    Root_Comp = 0
 
     Levi_Tot_Time = 0
     SotP_Tot_Time = 0
@@ -447,26 +576,41 @@ elif MODE == 4 and destinyMembershipId == 4611686018450697084:
 
         if Name == 2122313384:
             Name = "Last Wish"
+            LW_Start += 1
+            if Completion == 1:
+                LW_Comp += 1
             LW_Tot_Time += Time
             LW_Tot_K += Kills
             LW_Tot_D += Deaths
         elif Name == 3458480158 or Name == 2659723068:
             Name = "Garden of Salvation"
+            GoS_Start += 1
+            if Completion == 1:
+                GoS_Comp += 1
             GoS_Tot_Time += Time
             GoS_Tot_K += Kills
             GoS_Tot_D += Deaths
         elif Name == 910380154:
             Name = "Deep Stone Crypt"
+            DSC_Start += 1
+            if Completion == 1:
+                DSC_Comp += 1
             DSC_Tot_Time += Time
             DSC_Tot_K += Kills
             DSC_Tot_D += Deaths
         elif Name == 3881495763:
             Name = "Vault of Glass"
+            VoG_Start += 1
+            if Completion == 1:
+                VoG_Comp += 1
             VoG_Tot_Time += Time
             VoG_Tot_K += Kills
             VoG_Tot_D += Deaths
         elif Name == 1441982566:
             Name = "Vow of the Disciple"
+            Vow_Start += 1
+            if Completion == 1:
+                Vow_Comp += 1
             Vow_Tot_Time += Time
             Vow_Tot_K += Kills
             Vow_Tot_D += Deaths
@@ -475,21 +619,33 @@ elif MODE == 4 and destinyMembershipId == 4611686018450697084:
                 Name = "King's Fall"
             elif Name == 2964135793:
                 Name = "King's Fall: Master"
+            KF_Start += 1
+            if Completion == 1:
+                KF_Comp += 1
             KF_Tot_Time += Time
             KF_Tot_K += Kills
             KF_Tot_D += Deaths
         elif Name == 2381413764:
             Name = "Root of Nightmares"
+            Root_Start += 1
+            if Completion == 1:
+                Root_Comp += 1
             Root_Tot_Time += Time
             Root_Tot_K += Kills
             Root_Tot_D += Deaths
         elif Name == 2693136601:
             Name = "Leviathan"
+            Levi_Start += 1
+            if Completion == 1:
+                Levi_Comp += 1
             Levi_Tot_Time += Time
             Levi_Tot_K += Kills
             Levi_Tot_D += Deaths
         elif Name == 548750096:
             Name = "Scourge of the Past"
+            SotP_Start += 1
+            if Completion == 1:
+                SotP_Comp += 1
             SotP_Tot_Time += Time
             SotP_Tot_K += Kills
             SotP_Tot_D += Deaths
@@ -507,21 +663,27 @@ elif MODE == 4 and destinyMembershipId == 4611686018450697084:
         Raid_Tot_Deaths += Deaths
         Raid_Tot_Time += Time
 
-        print(Raids)
+        # print(Raids)
 
-    Seconds = Raid_Tot_Time % 60
-    Minutes = Raid_Tot_Time / 60
-    Minutes = math.trunc(Minutes)
-    Hours = Minutes / 60
-    Hours = math.trunc(Hours)
-    Minutes = Minutes % 60
-    Days = Hours / 24
-    Days = math.trunc(Days)
-    Hours = Hours % 24
-    Weeks = Days / 7
-    Weeks = math.trunc(Weeks)
-    Days = Days % 7
 
-    Raid_Tot_Time = (f'{Weeks} week(s), {Days} day(s), {Hours}:{Minutes}:{Seconds}')
+    print(f'Total Kills: {round(Raid_Tot_Kills)}\nTotal Deaths: {round(Raid_Tot_Deaths)}')
+    Time_Converter(Raid_Tot_Time)
 
-    print(Raid_Tot_Time, Raid_Tot_Kills, Raid_Tot_Deaths)
+    All_Starts = (Levi_Start, SotP_Start, LW_Start, GoS_Start, DSC_Start, VoG_Start, Vow_Start, KF_Start, Root_Start)
+    All_Comps = (Levi_Comp, SotP_Comp, LW_Comp, GoS_Comp, DSC_Comp, VoG_Comp, Vow_Comp, KF_Comp, Root_Comp)
+    All_Tot_K = (Levi_Tot_K, SotP_Tot_K, LW_Tot_K, GoS_Tot_K, DSC_Tot_K, VoG_Tot_K, Vow_Tot_K, KF_Tot_K, Root_Tot_K)
+    All_Tot_D = (Levi_Tot_D, SotP_Tot_D, LW_Tot_D, GoS_Tot_D, DSC_Tot_D, VoG_Tot_D, Vow_Tot_D, KF_Tot_D, Root_Tot_D)
+
+    All_Stats = (f'Raids Starts: {All_Starts}\nRaids Finished: {All_Comps}\n'
+                 f'Total Raid Kills: {All_Tot_K}\nTotal Raid Deaths: {All_Tot_D}')
+
+    print(All_Stats)
+    Time_Converter(Levi_Tot_Time)
+    Time_Converter(SotP_Tot_Time)
+    Time_Converter(LW_Tot_Time)
+    Time_Converter(GoS_Tot_Time)
+    Time_Converter(DSC_Tot_Time)
+    Time_Converter(VoG_Tot_Time)
+    Time_Converter(Vow_Tot_Time)
+    Time_Converter(KF_Tot_Time)
+    Time_Converter(Root_Tot_Time)
