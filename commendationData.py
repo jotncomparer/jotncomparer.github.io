7dcb870316774acd77acb1875c9df60b07ae5b08
Man in the Moon <139192900+Man-in-the-Moon@users.noreply.github.com>
2023-08-08 16:40:15 -0400

    Update commendationData.py

--- commendationData.py
+++ commendationData.py
@@ -1,6 +1,6 @@
 # Connor Downs
 # Started: 8-7-2023
-# Last Updated: 8-7-2023
+# Last Updated: 8-8-2023
 # This program needs Character_Metrics.py to work properly.
 
 # This program is used to define every player in Jötunn Gang.
@@ -9,6 +9,7 @@
 
 import json
 from prettytable import PrettyTable
+
 
 def commendationData():
     with open('Metrics.json') as f:
@@ -21,29 +22,62 @@ def commendationData():
     with open('Metrics.json', "w") as f:
         f.write(json.dumps(metric_json, indent=2))
 
+    guardianRank = metric_json["Response"][0]['profile']['data']['currentGuardianRank']
+    guardianRankTitle = 0
+    if guardianRank <= 6:
+        guardianRankTitle = 'Blueberry'
+    elif guardianRank == 7:
+        guardianRankTitle = 'Elite'
+    elif guardianRank == 8:
+        guardianRankTitle = 'Justiciar'
+    elif guardianRank == 9:
+        guardianRankTitle = 'Vanquisher'
+    elif guardianRank == 10:
+        guardianRankTitle = 'Exemplar'
+    elif guardianRank == 11:
+        guardianRankTitle = 'Paragon'
+
+    print(f'Guardian Rank: {guardianRank} - {guardianRankTitle}')
+
     # Commendation Score by Types
     allyScore = metric_json["Response"][0]['profileCommendations']['data']['commendationNodeScoresByHash']['154475713']
     funScore = metric_json["Response"][0]['profileCommendations']['data']['commendationNodeScoresByHash']['1341823550']
-    leadershipScore = metric_json["Response"][0]['profileCommendations']['data']['commendationNodeScoresByHash']['1390663518']
-    masteryScore = metric_json["Response"][0]['profileCommendations']['data']['commendationNodeScoresByHash']['4180748446']
+    leadershipScore = metric_json["Response"][0]['profileCommendations']['data']['commendationNodeScoresByHash'][
+        '1390663518']
+    masteryScore = metric_json["Response"][0]['profileCommendations']['data']['commendationNodeScoresByHash'][
+        '4180748446']
     # Commendation Percentage by Types
-    allyPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash']['154475713']
-    funPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash']['1341823550']
-    leadershipPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash']['1390663518']
-    masteryPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash']['4180748446']
+    allyPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash'][
+        '154475713']
+    funPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash'][
+        '1341823550']
+    leadershipPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash'][
+        '1390663518']
+    masteryPercent = metric_json["Response"][0]['profileCommendations']['data']['commendationNodePercentagesByHash'][
+        '4180748446']
     # Specific Commendation Cards
     selflessScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['354527503']
-    bestDressedScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['357212819']
-    primevalInstinctScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['363818544']
-    indispensableScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['2019871317']
-    patientConsiderateScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['2506835299']
-    levelHeadedScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['3037314846']
-    joyBringerScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['3377580220']
-    thoughfulScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['3513056018']
+    bestDressedScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '357212819']
+    primevalInstinctScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '363818544']
+    indispensableScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '2019871317']
+    patientConsiderateScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '2506835299']
+    levelHeadedScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '3037314846']
+    joyBringerScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '3377580220']
+    thoughfulScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '3513056018']
     heroicScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['3575743922']
-    perceptiveScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['3872064891']
-    knowledgeableScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['3970150545']
-    playmakerScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['4209356036']
+    perceptiveScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '3872064891']
+    knowledgeableScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '3970150545']
+    playmakerScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash'][
+        '4209356036']
     # totalScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['']
     # totalScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['']
     # totalScore = metric_json["Response"][0]['profileCommendations']['data']['commendationScoresByHash']['']
